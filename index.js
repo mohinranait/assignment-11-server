f1d498ef2d0ddd7a7fe2c9c637b39d9b3b47a2ce
Mohin Rana <ebrahimit49@gmail.com>
2023-11-05 18:18:59 +0600

    Pagination and filtering add

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 const dotenv = require('dotenv');
@@ -10,7 +10,10 @@ dotenv.config();
 
 // Middlewire
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin : ["http://localhost:5173"],
+  credentials: true,
+}));
 
 
 
@@ -32,7 +35,75 @@ async function run() {
     const database = client.db("assignment_10");
     const assignmentCollection = database.collection('assignments');
 
-    
+    // get all assignments
+    app.get("/api/v1/assignments", async (req, res) => {
+      const queryLevel = {};
+      const page = Number(req.query?.page)
+      const size = Number(req.query?.size)
+      console.log(page, size);
+      
+      const level = req.query?.level;
+      if(level){
+        queryLevel.level = level
+      }
+        const cursor =  assignmentCollection.find(queryLevel).skip(page*size).limit(size)
+        const result = await cursor.toArray();
+        const count = await assignmentCollection.estimatedDocumentCount();
+        res.send({
+          result,
+          count : Number(count)
+        });
+    })
+
+    // get assignment using ID
+    app.get("/api/v1/assignment/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+
+
+      const result = await assignmentCollection.findOne(filter)
+      res.send(result);
+  })
+
+    // Create assignments
+    app.post("/api/v1/create-assignment", async (req, res) => {
+        const assignment = req.body;
+        const result = await assignmentCollection.insertOne(assignment);
+        res.send(result);
+    })
+
+
+
+    // Create assignments
+    app.patch("/api/v1/update-assignment/:id", async (req, res) => {
+        const id = req.params?.id;
+        console.log(id);
+        const filter = {_id: new ObjectId(id)};
+        const assignment = req.body;
+
+        const updateDoc = {
+          $set: assignment
+        }
+
+        const result = await assignmentCollection.updateOne(filter, updateDoc);
+        res.send(result);
+    })
+
+    // user wish assignment get
+    app.get("/api/v1/my-assignment", async (req, res) => {
+      const email = req.query?.email;
+      const filter = {email: email};
+      const result = await assignmentCollection.find(filter).toArray();
+      res.send(result)
+    })
+
+    // Delete my assignment
+    app.delete("/api/v1/delete-my-assign/:id", async (req, res) => {
+      const id = req.params?.id;
+      const filter = {_id: new ObjectId(id) };
+      const result = await assignmentCollection.deleteOne(filter);
+      res.send(result)
+    })
 
 
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
